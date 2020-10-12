4450df1bf1fdead9f68701f15a30c4a14e0d590c
Dandy27 <danielsbarbosa27@gmail.com>
2020-10-12 19:16:01 -0300

    Add endereco models

--- core/models.py
+++ core/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
-
+from enderecos.models import Endereco
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    comentarios = models.ManyToManyField(Comentario)
     atracoes = models.ManyToManyField(Atracao)
     avaliacoes = models.ManyToManyField(Avaliacao)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    
 
     def __str__(self):
         return self.nome
