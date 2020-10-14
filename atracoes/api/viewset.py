3ac4bdd00674fb25d387296cd646cbd9288850c7
Dandy27 <danielsbarbosa27@gmail.com>
2020-10-13 23:47:28 -0300

    Add search filter

--- atracoes/api/viewset.py
+++ atracoes/api/viewset.py
@@ -5,3 +5,5 @@ from .serializers import AtracaoSerializer
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_fields = ('nome', 'descricao')
+    
--- core/api/viewset.py
+++ core/api/viewset.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
@@ -6,10 +7,27 @@ from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['^nome', '^descricao', '^endereco__linha1']
 
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+        
+        if nome:
+            queryset = queryset.filter(nome__iexact=nome)
+        
+        if descricao:
+            queryset = queryset.filter(descricao__iexact=descricao)
+
+
+        return queryset 
 
 
     def list(self, request, *args, **kwargs):
